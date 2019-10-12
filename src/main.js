3924171edfd9a9359b0f6cbd1d4903ea158f6302
helga-hai <o.hainyzhnyk@gmail.com>
2019-10-12 13:21:01 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
+console.log(Vuex);
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
