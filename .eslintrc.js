eae8c3f3a7b28b34f922e7fd89ff2b7677c2b70c
helga-hai <o.hainyzhnyk@gmail.com>
2019-10-13 07:48:42 +0300

    for heroku

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 8,
+        "parser": "babel-eslint"
+    },
+    extends: [
+        'plugin:vue/essential'
+    ],
+    rules: {}
+}
