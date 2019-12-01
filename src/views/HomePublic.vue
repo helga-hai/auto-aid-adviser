1f3d46bb9ff0ed90768da1082b44fd0dd502067d
1Karas <a.karas.vio@gmail.com>
2019-12-01 13:38:21 +0200

    with '#' will be ignored, and an empty message aborts the commit.

--- src/views/HomePublic.vue
+++ src/views/HomePublic.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="res" :class="{'res__login': loginShow}" :style="{'background-image': 'url(' + require('../assets/backgroundimage.jpg') + ')'}">
+  <div class = 'wrap' :class="{'res__login': loginShow}" :style="{'background-image': 'url(' + require('../assets/backgroundimage.jpg') + ')'}">
+    <nav-component />
+    <div class="res" >
     <div class="one">
       <label for="">Оберіть тип объекту</label  @click="loginShow = !loginShow">
       <button type="button" name="button"  @click="loginShow = !loginShow">
@@ -27,9 +29,56 @@
     </div>
     <div class="btn">
       <a href="#" class="button orange">Знайти</a>
-
     </div>
+    </div>
+    <div class="modal-login-registration active-modal">
+      <div class="close-x">
+        <span class="close">x</span>
+      </div>
 
+      <div class="entrance">
+        <div class="mode active">
+          <span>Вхід</span>
+        </div>
+        <div class="divider">
+          <span>&nbsp;|&nbsp;</span>
+        </div>
+        <div class="mode">
+          <span>Реєстрація</span>
+        </div>
+      </div>
+      <form class="login-form active-form" method="post">
+        <div class="form-input">
+          <input type="email" placeholder="E-mail">
+          <div class="error-message">
+            <span>Невірний формат e-mail</span>
+          </div>
+        </div>
+        <div class="form-input">
+          <input type="password" placeholder="Введіть пароль">
+          <div class="error-message">
+            <span>Невірний формат e-mail</span>
+          </div>
+        </div>
+        <button class="form-input submit-button">
+          Увійти
+        </button>
+      </form>
+      <div class="lost-password">
+          <a href="#">Забули пароль?</a>
+      </div>
+      <div class="media-login">
+        <p>Увійти за допомогою соцмереж</p>
+        <ul class="social-providers">
+          <li>
+            f
+          </li>
+          <li>
+            G
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,6 +102,9 @@ export default {
 button{
   border: 1px solid #000;
   border-radius: 4px;
+}
+.wrap {
+height: 100vh;
 }
 .res {
   /*background: url(require('@/assets/backgroundimage.jpg'));*/
@@ -228,5 +280,95 @@ a.button.orange {
   font-family: Roboto;
   border-radius: 4px;
   background-color: #FFC700;
+}
+body{
+  width: 800px;
+  border: 1px solid;
+  height: 100%;
+}
+div,input {
+  box-sizing: border-box;
+}
+.modal-login-registration {
+  padding: 32px 50px;
+  width: 400px;
+  position: absolute;
+  left: auto;
+  right: 0px;
+  top: 0;
+  bottom: 0px;
+  background-color: #FFFFFF;
+  transition: right 2s;
+
+}
+.modal-login-registration.active-modal {
+  right: 400px;
+}
+.close-x {
+  display: flex;
+  justify-content: flex-end;
+}
+.close-x .close {
+  display: block;
+  width: 15px;
+  height: 15px;
+}
+.close:hover {
+  cursor: pointer;
+}
+.entrance {
+  margin-top: 43px;
+  display: flex;
+  justify-content: flex-start;
+  align-content: center;
+}
+.entrance .mode, .entrance .divider {
+  display: flex;
+  align-content: center;
+}
+.entrance .mode span {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  color: #0E1E2E;
+  opacity: 0.5;
+}
+.entrance .mode.active {
+  opacity: 1;
+}
+.login-form {
+  margin-top: 26px;
+}
+.login-form .form-input {
+  margin-bottom: 36px;
+}
+.login-form .form-input input {
+  width: 300px;
+  height: 56px;
+  padding-left: 16px;
+  border: 1px solid #6F7E95;
+  border-radius: 4px;
+}
+.login-form .submit-button {
+  background: #FFC700;
+  border-radius: 4px;
+  padding-left: 0px;
+  text-align: center;
+  width: 300px;
+  height: 56px;
+  border: 0;
+}
+.lost-password {
+  margin-bottom: 36px;
+  text-align: center;
+}
+.media-login {
+  margin-bottom: 36px;
+  text-align: center;
+}
+.social-providers {
+  margin: 0px;
+  padding: 0px;
 }
 </style>
