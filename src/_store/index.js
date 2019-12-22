487c1def2d1ae441a629a18eb40bb0ada6a00056
Olha Hai-Nyzhnyk <olha@ribs.one>
2019-12-22 17:16:31 +0200

    create.module

--- src/_store/index.js
+++ src/_store/index.js
@@ -8,6 +8,7 @@ import { selfLocation } from './selfLocation.module';
 import { maploader } from './maploader.module';
 import { registration } from './registration.module';
 import { data } from './data.module';
+import { create } from './create.module';
 
 Vue.use(Vuex);
 
@@ -19,6 +20,7 @@ export const store = new Vuex.Store({
         selfLocation,
         maploader,
         registration,
-        data
+        data,
+        create
     }
 });
