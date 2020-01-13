c2b0578736d7ddc9c1f4ba0734841f8fa18d4eea
helga-hai <o.hainyzhnyk@gmail.com>
2020-01-13 21:37:03 +0200

    axios

--- src/_store/templateB.module.js
+++ src/_store/templateB.module.js
@@ -15,9 +15,15 @@ export const templateB = {
             bite: [],
             scream: []
         },
-        test: [
-
-        ]
+        dataList: []
+    },
+    getters: {
+        SERVICE: state => {
+            return state.service; //let name = this.$store.getters.NAME
+        },
+        DATALIST: state => {
+            return state.dataList; //let name = this.$store.getters.NAME
+        },
     },
     actions: {
         // getAll({ commit }) {
@@ -67,10 +73,44 @@ export const templateB = {
 
             // state.st = getUnique(state.serviceTypesObj, 'id')
         },
+        GET_DATALIST: async(context, payload) => {
+            console.log('GET_DATALIST')
+            const requestOptions = {
+                //method: 'GET',
+                headers: authHeader()
+            };
+            const config = {
+                method: 'get',
+                url: userService.config.apiUrl + '/api/catalog/services',
+                headers: authHeader()
+            }
+            let { data } = await axios(config);
+            context.commit('SET_DATALIST', data);
+        },
+
+        SAVE_DATALIST: async(context, payload) => {
+            console.log('SAVE_DATALIST')
+            let { data } = await axios.post(userService.config.apiUrl + '/api/catalog/services', requestOptions);
+            context.commit('ADD_DATALIST', payload);
+        },
     },
     mutations: {
+        SET_DATALIST: (state, payload) => {
+            console.log('SET_DATALIST')
+            state.dataList = payload;
+        },
+        ADD_DATALIST: (state, payload) => {
+            console.log('ADD_DATALIST')
+            state.datsList.push(payload);
+        },
         fillallBusinesServises(state, payload) {
             state.allBusinesServises = payload;
+
+            //
+            payload.filter(item => {
+
+                })
+                //
             payload.forEach(item => {
                 if (state.serviceTypesList.indexOf(item.serviceType.name) < 0) {
                     state.serviceTypesList.push(item.serviceType.name)
--- src/views/CreatePage2.vue
+++ src/views/CreatePage2.vue
@@ -24,6 +24,17 @@ export default {
         BusinessLayout,
         CardMin,
     },
+    mounted() {
+        this.$store.dispatch('templateB/GET_DATALIST');
+    },
+    computed: {
+        dataList() {
+            return this.$store.getters.DATALIST; 
+            // import {mapGetters} from 'vuex';
+            // ...mapGetters(['DATALIST']), 
+            // <div class="todo-item" v-for="item in DATALIST"></div>
+        },
+    },
     methods: {
         createStepOne() {
             this.$emit('stepOneFunction');
