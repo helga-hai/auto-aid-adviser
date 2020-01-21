0d9c6d09f38dfbd6c583f93897ac4898efd8cbe8
helga-hai <o.hainyzhnyk@gmail.com>
2020-01-21 23:17:20 +0200

    serveices uri

--- src/_store/templateB.module.js
+++ src/_store/templateB.module.js
@@ -34,10 +34,10 @@ export const templateB = {
             state.commit('fillallBusinesServises', payload);
         },
         GET_CATTEGORY_LIST: async(context, payload) => {
-            console.log('GET_CATTEGORY_LIST')
+            console.log('GET_CATTEGORY_LIST', userService.config.apiUrl + '/api/catalog/business/type/17/service/types')
             const config = {
                 method: 'get',
-                url: userService.config.apiUrl + '/api/catalog/business/type/1/service/types',
+                url: userService.config.apiUrl + '/api/catalog/business/type/17/service/types',
                 headers: authHeader()
             }
             let { data } = await axios(config);
